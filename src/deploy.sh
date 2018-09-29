51b82f54e9574c4a1ef59fa7c957f166447f7718
Mateo.Martin <Mateo.Martin@protonmail.com>
2018-09-29 15:49:01 +0800

    remove quotes

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-USER="$1"
-PASS="$2"
-TAG="$3"
-ARCH="$4"
+USER=$1
+PASS=$2
+TAG=$3
+ARCH=$4
 
-ORG="elespejo"
-REPO="sskcp-${ARCH}"
+ORG=elespejo
+REPO=sskcp-$ARCH
 
 
 docker tag $REPO $ORG/$REPO:$TAG 
-docker login -u "$USER" -p "$PASS"
+docker login -u $USER -p $PASS
 docker push $ORG/$REPO:$TAG
