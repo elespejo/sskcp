db194280a26140df4d02d95d4a1142c5a6651e9f
Mateo.Martin <Mateo.Martin@protonmail.com>
2018-09-29 15:35:31 +0800

    add build process for docker image armv6

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cur_dir=`cd $(dirname $0);pwd`
-docker build -f $cur_dir/Dockerfile -t sskcp-x86 ./src
+docker build -f $cur_dir/Dockerfile-$1 -t sskcp-$1 ./src
--- src/deploy.sh
+++ src/deploy.sh
@@ -3,9 +3,10 @@
 USER="$1"
 PASS="$2"
 TAG="$3"
+ARCH="$4"
 
 ORG="elespejo"
-REPO="sskcp-x86"
+REPO="sskcp-${ARCH}"
 
 
 docker tag $REPO $ORG/$REPO:$TAG 
