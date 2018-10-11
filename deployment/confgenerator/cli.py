f291424afe83467c0f5227056bd88b18048c84b7
Mateo.Martin <Mateo.Martin@protonmail.com>
2018-10-11 11:28:11 +0800

    add cli

--- deployment/confgenerator/cli.py
+++ deployment/confgenerator/cli.py
@@ -5,18 +5,19 @@ def cli():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--ssport', dest="ssport", type=int, help='ss port')
     parser.add_argument('--kcpport', dest='kcpport', type=int, help='kcp port')
-    parser.add_argument('--dest', dest="dest", type=int, help='dest')
-    parser.add_argument('--vpsip', dest='vpsip', type=int, help='vps ip')
+    parser.add_argument('--dest', dest="dest", help='dest')
+    parser.add_argument('--vpsip', dest='vpsip', help='vps ip')
+    return parser
 
 if __name__ == "__main__":
-    args = vars(parser.parse_args())
+    args = vars(cli().parse_args())
     if args["vpsip"] == None:
         print("Wrong")
     elif args["kcpport"] == None:
         ss = genss.SS(args) 
         runner = gen.Generation().add(ss)
-        runner.start("../conf")
+        runner.generate("../conf")
     else:
         sskcp = gensskcp.SSKCP(args)
         runner = gen.Generation().add(sskcp)
-        runner.start("../conf")
+        runner.generate("../conf")
